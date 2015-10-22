725167ec22808c6c472d7bbc7af215d3c30ef9c6
Ashwin Oke <okeashwin@gmail.com>
2015-10-22 02:36:24 -0400

    Added the test case generator script

---
+++ genTestCases.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ -f "evosuite-1.0.1.jar" ]
+then
+echo "==============Evosuite jar already exists================"
+echo "==============Skipping the download======================"
+else
+echo "==============Downloading the Evosuite jar==============="
+wget http://evosuite.org/files/evosuite-1.0.1.jar
+fi
+
+echo "==============Compiling the source======================="
+javac ./src/main/java/BankAccount.java
+
+echo "=====Generating the test cases for the compiled source========"
+cd ./src/test/java
+java -jar ../../../evosuite-1.0.1.jar -class BankAccount -projectCP ~/JenkinsOnEC2MavenProject/src/main/java/
+
+echo "==============Successfully completed=========================="
+exit 0
