b5668c79541f5d0ad4516aa7459e43d823d2c1ca
Nisarg <nisarg.gandhi64@gmail.com>
2015-10-23 19:53:39 -0400

    CheckKeys script added

--- checkKeys.sh
+++ checkKeys.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-files=$(git diff --name-only --cached) 
+files=$(git diff --name-only; git diff --name-only --cached)
+pattern="[^[:space:]]\{40,64\}"
+
+for i in $files; do
+	if grep -q $pattern $i; then
+		echo "$i contains AWS/DigitalOcean secret config key or private ssh key"
+		exit 1	
+	fi
+done 
